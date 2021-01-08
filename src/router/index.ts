77ee5f79910924535e2256e6aaed5f636693da75
fuzumin <fuzumin@outlook.com>
2021-01-08 17:57:46 +0800

    初始化

---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+// // const routes:RouteRecordRaw[] = [
+// const routes = [
+//   {
+
+//   }
+// ]
+
+// export default createRouter({
+//   history: createWebHistory(),
+//   routes
+// })
