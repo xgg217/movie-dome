98d1ed35f9517fdb1b4e9a3a74464530081258c5
fuzumin <fuzumin@outlook.com>
2021-01-13 10:36:23 +0800

    添加vant样式css文件

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,7 @@ import router from "./router";
 import App from './App.vue'
 import "./assets/styles/reset.css"
 // import 'vant/lib/index.css';
+import "./assets/styles/vant.css"
+
 
 createApp(App).use(router).mount('#app')
