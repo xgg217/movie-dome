35b19d785a35624eff25f7423c784af2c064bef9
fuzumin <fuzumin@outlook.com>
2021-01-16 11:47:49 +0800

    设置路径

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,9 @@
+const path = require('path');
+
 module.exports = {
+  alias: {
+    '/@/': path.resolve(__dirname, './src'),
+  },
   proxy: {
     "/api": {
       target: "http://47.110.74.37:7001/"
