da63127ae9aece218a2c99ce9920b837a4fef3ed
fuzumin <fuzumin@outlook.com>
2021-01-22 15:12:39 +0800

    更换搜索页面路径

--- src/router/index.ts
+++ src/router/index.ts
@@ -24,7 +24,7 @@ const routes:RouteRecordRaw[] = [
         component: () => import("/@/views/Rank/Index.vue")
       }, {
         path: "search",
-        component: () => import("/@/views/Search.vue")
+        component: () => import("/@/views/Search/Index.vue")
       },
     ]
   }, {
